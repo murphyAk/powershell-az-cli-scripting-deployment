fcc2e741ebae43d7057283ff2c3324a49647be26
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-08-10 16:18:12 -0400

    met requirement to run api

--- provisionResources.ps1
+++ provisionResources.ps1
@@ -1,21 +1,33 @@
 # TODO: set variables
-$studentName = ""
-$rgName = ""
-$vmName = ""
-$vmSize = ""
-$vmImage = ""
-$vmAdminUsername = ""
+$studentName = "andrew"
+$rgName = "$studentName-ps-st-rg"
+$vmName = "$studentName-ps-st-vm"
+$vmSize = "Standard_B2s"
+$vmImage = "Canonical:UbuntuServer:18.04-LTS:latest"
+$vmAdminUsername = "student"
+$vmAdminPassword = "LaunchCode-@zure1"
 $kvName = "$studentName-lc0820-ps-kv"
 $kvSecretName = "ConnectionStrings--Default"
 $kvSecretValue = "server=localhost;port=3306;database=coding_events;user=coding_events;password=launchcode"
 
 # TODO: provision RG
 
+az configure --default location=eastus
+az group create -n "$rgName"
+az configure --default group="$rgName"
+
 # TODO: provision VM
+
+az vm create -n $vmName --size "$vmSize" --image "$vmImage" --admin-username "$vmAdminUsername" --admin-password "$vmAdminPassword" --assign-identity --generate-ssh-keys
+az configure --default vm=$vmName
 
 # TODO: capture the VM systemAssignedIdentity
 
+$vmSysId = az vm show --query "identity.principalId"
+
 # TODO: open vm port 443
+
+az vm open-port --port 443
 
 # provision KV
 
@@ -23,7 +35,11 @@ az keyvault create -n $kvName --enable-soft-delete false --enabled-for-deploymen
 
 # TODO: create KV secret (database connection string)
 
+az keyvault secret set --vault-name "$kvName" -n "ConnectionStrings--Default" --value "server=localhost;port=3306;database=coding_events;user=coding_events;password=launchcode"
+
 # TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
+
+az keyvault set-policy -n "$kvName" --object-id "$vmSysId" --secret-permissions get list
 
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/1configure-vm.sh
 
@@ -33,3 +49,5 @@ az vm run-command invoke --command-id RunShellScript --scripts @deliver-deploy.s
 
 
 # TODO: print VM public IP address to STDOUT or save it as a file
+
+az vm show --show-details --query "publicIps"
